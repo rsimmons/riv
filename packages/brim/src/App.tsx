74b1c5acddc9eae13a7596a61df95060ab168f30
Russel Simmons <russ@rsimmons.org>
2019-08-27 15:58:47 -0700

    Add note about new escape key

--- packages/brim/src/App.tsx
+++ packages/brim/src/App.tsx
@@ -15,6 +15,7 @@ const App: React.FC = () => {
           <li>Right/left arrows move selection in and out of nested structures.</li>
           <li>Enter on an expression (or expression sub-tree) will begin editing it. Pressing enter again will stop editing.</li>
           <li>Instead of pressing enter, you can just start typing letters/numbers and it will begin the edit (overwriting what it there).</li>
+          <li>Escape aborts the current edit.</li>
           <li>Shift-enter (or comma) adds a new expression (or array item) below the current one.</li>
           <li>Pressing the = key on on an expression will move to editing its name/label.</li>
           <li>A red box indicates an undefined expression.</li>
