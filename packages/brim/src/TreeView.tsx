2c38542760d597e06a905aaf28413cd4fcaff3aa
Russel Simmons <russ@rsimmons.org>
2019-10-07 15:19:22 -0700

    Clean up TreeView code a bit

--- packages/brim/src/TreeView.tsx
+++ packages/brim/src/TreeView.tsx
@@ -16,24 +16,20 @@ export interface TreeViewContextData {
 const TreeViewContext = createContext<TreeViewContextData | null>(null);
 export const TreeViewContextProvider = TreeViewContext.Provider;
 
-interface StyleOptions {
-  backgroundColor: string;
-}
-
-interface GeneralNodeProps {
+interface AppishNodeProps {
   mainText: React.ReactNode;
-  styleOptions: StyleOptions;
+  boxColor: string;
   selected: boolean;
-  childCxns: boolean;
+  childCxns?: boolean;
   onSelect: () => void;
-  children: ReadonlyArray<{
+  children?: ReadonlyArray<{
     key: string | number | undefined,
     name: string | null,
     child: React.ReactNode}
   >;
 }
 
-const GeneralNode: React.FC<GeneralNodeProps> = ({mainText, styleOptions, selected, childCxns, onSelect, children = []}) => {
+const AppishNode: React.FC<AppishNodeProps> = ({mainText, boxColor, selected, childCxns = true, onSelect, children = []}) => {
   const boxClasses: Array<string> = [];
   const [hovered, setHovered] = useState(false);
 
@@ -65,17 +61,17 @@ const GeneralNode: React.FC<GeneralNodeProps> = ({mainText, styleOptions, select
   if (children.length > 0) {
     const rows = 2*children.length - 1;
     return (
-      <div className="TreeView-general-node TreeView-general-node-with-children">
-        <div style={{gridRowStart: 1, gridRowEnd: rows+1, gridColumnStart: 1, gridColumnEnd: 3, backgroundColor: styleOptions.backgroundColor }} />
-        <div className="TreeView-general-node-main-text TreeView-general-node-padding TreeView-general-node-no-pointer" style={{gridRowStart: 1, gridRowEnd: rows+1, gridColumn: 1}}>{mainText}</div>
+      <div className="TreeView-appish-node TreeView-appish-node-with-children">
+        <div style={{gridRowStart: 1, gridRowEnd: rows+1, gridColumnStart: 1, gridColumnEnd: 3, backgroundColor: boxColor }} />
+        <div className="TreeView-appish-node-main-text TreeView-appish-node-padding TreeView-appish-node-no-pointer" style={{gridRowStart: 1, gridRowEnd: rows+1, gridColumn: 1}}>{mainText}</div>
         <>{children.map(({key, name, child}, idx) => (
           <React.Fragment key={key}>
             {(idx > 0) ? (
-              <div className="TreeView-general-node-spacer-row" style={{gridRow: 2*idx, gridColumnStart: 1, gridColumnEnd: 5}} />
+              <div className="TreeView-appish-node-spacer-row" style={{gridRow: 2*idx, gridColumnStart: 1, gridColumnEnd: 5}} />
             ) : null}
-            <div className={name ? "TreeView-general-node-child-name TreeView-general-node-padding TreeView-general-node-no-pointer" : ''} style={{gridRow: 2*idx+1, gridColumn: 2}}>{name}</div>
-            <div className={childCxns ? 'TreeView-general-node-child-cxn' : ''} style={{gridRow: 2*idx+1, gridColumn: 3}}><div className="TreeView-general-node-child-cxn-inner" /></div>
-            <div className="TreeView-general-node-child-subtree" style={{gridRow: 2*idx+1, gridColumn: 4}}>{child}</div>
+            <div className={name ? "TreeView-appish-node-child-name TreeView-appish-node-padding TreeView-appish-node-no-pointer" : ''} style={{gridRow: 2*idx+1, gridColumn: 2}}>{name}</div>
+            <div className={childCxns ? 'TreeView-appish-node-child-cxn' : ''} style={{gridRow: 2*idx+1, gridColumn: 3}}><div className="TreeView-appish-node-child-cxn-inner" /></div>
+            <div className="TreeView-appish-node-child-subtree" style={{gridRow: 2*idx+1, gridColumn: 4}}>{child}</div>
           </React.Fragment>
         ))}
         </>
@@ -84,8 +80,8 @@ const GeneralNode: React.FC<GeneralNodeProps> = ({mainText, styleOptions, select
     );
   } else {
     return (
-      <div className="TreeView-general-node">
-        <div className={boxClasses.concat(['TreeView-general-node-padding']).join(' ')} style={{backgroundColor: styleOptions.backgroundColor}} onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+      <div className="TreeView-appish-node">
+        <div className={boxClasses.concat(['TreeView-appish-node-padding']).join(' ')} style={{backgroundColor: boxColor}} onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
           {mainText}
         </div>
       </div>
@@ -99,116 +95,85 @@ export const NodeView: React.FC<{node: Node}> = ({ node }) => {
     throw new Error();
   }
 
-  const mainText: React.ReactNode = (() => {
-    switch (node.type) {
-      case 'UndefinedLiteral':
-        return <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+  const children: ReadonlyArray<Node> = node.children; // to get type right
+  const selected = (ctxData.selectedNode === node);
 
-      case 'ArrayLiteral':
-        return '[]';
-
-      case 'UserFunctionDefinition':
-        return 'ƒ';
-
-      case 'UserFunctionDefinitionParameters':
-      case 'UserFunctionDefinitionExpressions':
-        return '\u2009'; // thin space, to ensure these are full height
-
-      case 'StreamIndirection':
-      case 'StreamParameter':
-        return <em>{node.name}</em>;
-
-      case 'NumberLiteral':
-        return node.value.toString();
-
-      case 'StreamReference': {
-        const targetExpressionNode = ctxData.streamIdToNode.get(node.targetStreamId);
-        if (!targetExpressionNode) {
-          throw new Error();
-        }
-        return isNamedNode(targetExpressionNode) ? targetExpressionNode.name : '<stream ' + node.targetStreamId + '>';
-      }
-
-      case 'Application': {
-        const functionNode = ctxData.functionIdToNode.get(node.functionId);
-        if (!functionNode) {
-          throw new Error();
-        }
-        return <strong>{functionNode.name ? functionNode.name : '<function ' + node.functionId + '>'}</strong>
-      }
-
-      default:
-        throw new Error();
-    }
-  })();
-
-  const children: ReadonlyArray<Node> = node.children;
-  const childrenProp = (() => {
-    switch (node.type) {
-      case 'Application': {
-        const functionNode = ctxData.functionIdToNode.get(node.functionId);
-        if (!functionNode) {
-          throw new Error();
-        }
-        if (functionNode.signature.parameters.length !== node.children.length) {
-          throw new Error('params and args length mismatch');
-        }
-        return functionNode.signature.parameters.map((param, idx) => ({
-          key: param.name,
-          name: param.name.startsWith('_') ? null : param.name,
-          child: <NodeView node={node.children[idx]} />
-        }));
-      }
-
-      default:
-        return children.map((child, idx) => ({
-          key: idx,
-          name: null,
-          child: <NodeView node={child} />
-        }));
-    }
-  })();
-
-  const backgroundColor = (() => {
-    switch (node.type) {
-      case 'UndefinedLiteral':
-        return 'red';
-
-      case 'UserFunctionDefinition':
-        return '#d3c9d8';
-
-      case 'UserFunctionDefinitionParameters':
-        return '#efd5db';
-
-      case 'UserFunctionDefinitionExpressions':
-          return '#f1e0cc';
-
-      case 'NumberLiteral':
-        return '#cce8cc';
-
-      case 'StreamReference':
-      case 'StreamIndirection':
-      case 'StreamParameter':
-        return '#ccd9e8';
-
-      default:
-        return '#d8d8d8';
-    }
-  })();
-
-  const styleOptions = {
-    backgroundColor,
-  };
+  if (selected && ctxData.mainState.editingSelected) {
+    return <ExpressionChooser mainState={ctxData.mainState} dispatch={ctxData.dispatch} />
+  }
 
   const handleSelect = () => {
     ctxData.onSelectNode(node);
   };
 
-  const selected = (ctxData.selectedNode === node);
+  const commonProps = {
+    selected,
+    onSelect: handleSelect,
+  };
 
-  if (selected && ctxData.mainState.editingSelected) {
-    return <ExpressionChooser mainState={ctxData.mainState} dispatch={ctxData.dispatch} />
-  } else {
-    return <GeneralNode mainText={mainText} selected={selected} childCxns={node.type !== 'UserFunctionDefinition'} onSelect={handleSelect} styleOptions={styleOptions} children={childrenProp} />
+  const normalBoxColor = '#d8d8d8';
+  const streamNameishColor = '#ccd9e8';
+
+  const makeAnonChildrenViews = () => children.map((child, idx) => ({
+    key: idx,
+    name: null,
+    child: <NodeView node={child} />
+  }));
+
+  switch (node.type) {
+    case 'UndefinedLiteral':
+      return <AppishNode mainText={<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>} boxColor={'red'} {...commonProps} />
+
+    case 'NumberLiteral':
+      return <AppishNode mainText={node.value.toString()} boxColor="#cce8cc" {...commonProps} />
+
+    case 'ArrayLiteral':
+      return <AppishNode mainText="[ ]" boxColor={normalBoxColor} children={makeAnonChildrenViews()} {...commonProps} />
+
+    case 'Application': {
+      const functionNode = ctxData.functionIdToNode.get(node.functionId);
+      if (!functionNode) {
+        throw new Error();
+      }
+      const displayedName = functionNode.name ? functionNode.name : ('<function ' + node.functionId + '>');
+
+      if (functionNode.signature.parameters.length !== node.children.length) {
+        throw new Error('params and args length mismatch');
+      }
+      const childrenViews = functionNode.signature.parameters.map((param, idx) => ({
+        key: param.name,
+        name: param.name.startsWith('_') ? null : param.name,
+        child: <NodeView node={node.children[idx]} />
+      }));
+
+      return <AppishNode mainText={<strong>{displayedName}</strong>} boxColor={normalBoxColor} children={childrenViews} {...commonProps} />
+    }
+
+    case 'StreamReference': {
+      const targetExpressionNode = ctxData.streamIdToNode.get(node.targetStreamId);
+      if (!targetExpressionNode) {
+        throw new Error();
+      }
+      const displayedName = isNamedNode(targetExpressionNode) ? targetExpressionNode.name : ('<stream ' + node.targetStreamId + '>');
+      return <AppishNode mainText={displayedName} boxColor={streamNameishColor} {...commonProps} />
+    }
+
+    case 'StreamIndirection':
+      return <AppishNode mainText={<em>{node.name}</em>} boxColor={streamNameishColor} children={makeAnonChildrenViews()} {...commonProps} />
+
+    case 'StreamParameter':
+      return <AppishNode mainText={<em>{node.name}</em>} boxColor={streamNameishColor} {...commonProps} />
+
+    case 'UserFunctionDefinition':
+      return <AppishNode mainText="ƒ" boxColor="#d3c9d8" childCxns={false} children={makeAnonChildrenViews()} {...commonProps} />
+
+    case 'UserFunctionDefinitionParameters':
+      return <AppishNode mainText={'\u2009'} boxColor="#efd5db" children={makeAnonChildrenViews()} {...commonProps} />
+
+    case 'UserFunctionDefinitionExpressions':
+      return <AppishNode mainText={'\u2009'} boxColor="#f1e0cc" children={makeAnonChildrenViews()} {...commonProps} />
+
+    default:
+      throw new Error();
   }
 }
