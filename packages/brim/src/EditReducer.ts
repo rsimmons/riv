64b296394ca13367fdfec80cb8985b7c4d12796d
Russel Simmons <russ@rsimmons.org>
2020-01-23 18:07:31 -0800

    Set referent for params

--- packages/brim/src/EditReducer.ts
+++ packages/brim/src/EditReducer.ts
@@ -272,7 +272,7 @@ export function getReferentOfSelected(selTree: SelTree, envLookups: EnvironmentL
         return streamDef.expr;
 
       case 'param':
-        return undefined; // TODO: can we return something useful?
+        return streamDef.param;
 
       default: {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
